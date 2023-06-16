556c097f71bc546223f1d86a83b74ce4b2cd627e
Tasneem <thajudeentasneem@gmail.com>
2023-06-16 18:22:36 +0800

    Return values for bmi.py

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height,weight):
 
     if bmi<18.5:
         print("Under Weight")
+        return -1
     elif bmi>25.0:
         print("Over Weight")
+        return 1
     else:
         print("Normal Weight")
+        return 0
 
 calculate_bmi(weight=55,height=1.74)
